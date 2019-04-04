a0fbf9f0683cc9b0b61932873ae208690726ca07
Calvin Wong <calwong42991@gmail.com>
2019-04-04 14:17:43 -0700

    hashtable initial commit

---
+++ Data-Structures/hash-table/hashTable.js
@@ -0,0 +1,13 @@
+const LinkedList = require('../linked-list/LinkedListClass');
+
+const defaultHashTableSize = 32;
+
+class HashTable {
+  constructor(HashTableSize = defaultHashTableSize) {
+
+  }
+
+
+}
+
+module.exports = HashTable;
