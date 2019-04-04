9e12efbd1f740833765b6da714f70d5cb64740ac
Calvin Wong <calwong42991@gmail.com>
2019-04-03 19:11:22 -0700

    linkedlist node complete

--- Data-Structures/linked-list/linked-list-helper/LinkedListNodeFN.js
+++ Data-Structures/linked-list/linked-list-helper/LinkedListNodeFN.js
@@ -1,5 +1,14 @@
-const LinkedListNode = () => {
+const LinkedListNode = (val, nxt) => {
+  let value = val;
+  let next = nxt || null;
 
+  const toString = (callback) => {
+    return callback ? callback(value) : `${value}`;
+  }
+
+  return {
+    toString
+  }
 }
 
 module.exports = LinkedListNode;
