c34473a7911f1ed92681507f2eee94ded16ebd5e
Calvin Wong <calwong42991@gmail.com>
2019-04-05 14:29:42 -0700

    doubly linked list first commit

---
+++ Data-Structures/doubly-linked-list/DoublyLinkedList.js
@@ -0,0 +1,10 @@
+const DoublyLinkedListNode = require('./DoublyLinkedListNode');
+const Comparator = require('../../Utils/comparator/ComparatorClass');
+
+class DoublyLinkedList {
+  constructor() {
+
+  }
+}
+
+module.exports = DoublyLinkedList;
