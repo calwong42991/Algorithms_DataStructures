cb6fc1da17ae591960123776bf6edc01d856b01a
Calvin Wong <calwong42991@gmail.com>
2019-04-05 17:27:01 -0700

    Graph added function names

---
+++ Data-Structures/graph/Graph.js
@@ -0,0 +1,33 @@
+class Graph {
+  constructor(isDirected = false) {
+    this.vertices = {};
+    this.edges = {};
+    this.isDirected = isDirected;
+  }
+
+  addVertex() {};
+
+  getVertexByKey() {};
+
+  getNeighbors() {};
+
+  getAllEdges() {};
+
+  addEdge() {};
+
+  deleteEdge() {};
+
+  findEdge() {};
+
+  reverse() {};
+
+  getVerticesIndices() {};
+
+  getAdjacencyMatrix() {};
+
+  toString() {};
+
+
+}
+
+module.exports = Graph;
