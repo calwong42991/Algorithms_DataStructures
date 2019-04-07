ca9bac8839652969dcfcce394421098768e604ee
Calvin Wong <calwong42991@gmail.com>
2019-04-06 21:44:27 -0700

    added Function skeleton to binary tree Node

--- Data-Structures/tree/BinarySearchTree.js
+++ Data-Structures/tree/BinarySearchTree.js
@@ -3,9 +3,39 @@ const HashTable = require('../hash-table/hashTable');
 
 
 class BinaryTreeNode {
-  constructor() {
+  constructor(value = null) {
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.value = value;
 
+    this.meta = new HashTable();
+    this.nodeComparator = new Comparator();
   }
+
+  get leftHeight() {};
+
+  get rightHeight() {};
+
+  get height() {};
+
+  get balanceFactor() {};
+
+  get uncle() {};
+
+  setValue(node) {};
+
+  setRight() {};
+
+  removeChild(nodeToRemove) {};
+
+  replaceChild() {};
+
+  static copyNode() {};
+
+  traverseInOrder() {};
+
+  toString() {};
 }
 
 module.exports = BinaryTreeNode;
