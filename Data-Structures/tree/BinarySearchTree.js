0ad87dde7db74c7f31c338599256036d8fd1ab4a
Calvin Wong <calwong42991@gmail.com>
2019-04-06 21:39:16 -0700

    Binary Tree Node initial commit

---
+++ Data-Structures/tree/BinarySearchTree.js
@@ -0,0 +1,11 @@
+const Comparator = require('../../Utils/comparator/ComparatorClass');
+const HashTable = require('../hash-table/hashTable');
+
+
+class BinaryTreeNode {
+  constructor() {
+
+  }
+}
+
+module.exports = BinaryTreeNode;
