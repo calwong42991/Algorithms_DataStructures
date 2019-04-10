fbb1a67e0d328024243786a29586df2b21fadef9
Calvin Wong <calwong42991@gmail.com>
2019-04-09 20:37:49 -0700

    power of two bitwise solution complete

--- Algorithms/is-power-of-two/isPowerOfTwoBitwise.js
+++ Algorithms/is-power-of-two/isPowerOfTwoBitwise.js
@@ -1,5 +1,9 @@
-const isPowerOfTwoBitwise = () => {
+const isPowerOfTwoBitwise = (number) => {
+  if (number < 1){
+    return false;
+  }
 
+  return (number & (number - 1)) === 0;
 };
 
 module.exports = isPowerOfTwoBitwise;
