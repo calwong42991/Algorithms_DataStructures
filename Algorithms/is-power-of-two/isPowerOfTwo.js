9a16744072b674abe552bb5a71ea61dc348d3010
Calvin Wong <calwong42991@gmail.com>
2019-04-09 20:32:10 -0700

    power of two naive solution

--- Algorithms/is-power-of-two/isPowerOfTwo.js
+++ Algorithms/is-power-of-two/isPowerOfTwo.js
@@ -1,5 +1,19 @@
-const isPowerOfTwo = () => {
+const isPowerOfTwo = (number) => {
 
+  if(number < 1){
+    return false;
+  }
+
+  let dividedNumber = number;
+
+  while(dividedNumber !== 1){
+    if(dividedNumber % 2 !== 0){
+      return false;
+    }
+
+    dividedNumber /= 2;
+  }
+  return true;
 };
 
 module.exports = isPowerOfTwo;
