e76786cda61e41ce8fecdec00c4c8c8fc38d9fd6
Calvin Wong <calwong42991@gmail.com>
2019-04-12 18:55:10 -0700

    fast powering complete

--- Algorithms/fast-powering/fastPowering.js
+++ Algorithms/fast-powering/fastPowering.js
@@ -1,5 +1,15 @@
-const fastPowering = () => {
+const fastPowering = (base, power) => {
+  if(power === 0){
+    return 1;
+  }
 
+  if(power % 2 === 0){
+    const multiplier = fastPowering(base, power / 2);
+    return multiplier * multiplier;
+  }
+
+  const multiplier = fastPowering(base, Math.floor(power / 2));
+  return multiplier * multiplier * base;
 };
 
 module.exports = fastPowering;
