4c0320bb4572af81b2ac51dba18094047643b877
Calvin Wong <calwong42991@gmail.com>
2019-04-15 22:47:42 -0700

    .

--- Algorithms/Euclidean Algorithm/euclideanAlgorithmIteractive.js
+++ Algorithms/Euclidean Algorithm/euclideanAlgorithmIteractive.js
@@ -2,7 +2,7 @@ const euclideanAlgorithmInteractive = (originalA, originalB) => {
   let a = Math.abs(originalA);
   let b = Math.abs(originalB);
 
-  while(a && b && a !== b){
+  while (a && b && a !== b) {
     [a, b] = a > b ? [a - b, b] : [a, b - a];
   }
 
