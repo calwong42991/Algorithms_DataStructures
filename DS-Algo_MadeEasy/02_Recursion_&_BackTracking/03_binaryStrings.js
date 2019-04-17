7fd30ca86b8295e6a022d3a4fa8d1f300dd581b5
unknown <calwong42991@gmail.com>
2019-04-17 16:43:32 -0700

    backtracking binary strings complete

---
+++ DS-Algo_MadeEasy/02_Recursion_&_BackTracking/03_binaryStrings.js
@@ -0,0 +1,16 @@
+const binary = (n) => {
+
+    const arr = [];
+  
+    let recursion = (n) => {
+      if(n < 1){
+        console.log(arr);
+      } else {
+        arr[n - 1] = 0;
+        recursion(n - 1);
+        arr[n - 1] = 1;
+        recursion(n - 1);
+      }
+    }
+    recursion(n);
+  }
