b8b1c20175676d41c37a355695061aaec30787bd
unknown <calwong42991@gmail.com>
2019-04-19 13:45:02 -0700

    hash table find nth complete

--- DS-Algo_MadeEasy/03_LinkedList/Problems/02_findNthNode.js
+++ DS-Algo_MadeEasy/03_LinkedList/Problems/02_findNthNode.js
@@ -11,7 +11,7 @@ const findNth = (linkedList, n) => {
         count += 1;
     }
 
-    console.log(count);
+    //console.log(count);
 
     if(count <=  n - 1){
         return `fewer number of nodes in the list`
@@ -19,12 +19,30 @@ const findNth = (linkedList, n) => {
     
     current = linkedList.head;
 
-    for(let i = 0; i < count - n ; i++){
+    for(let i = 0; i < count - n; i++){
         current = current.next;
     }
     
-    return current;
+    return current.value;
 
+}
+
+// hash table
+
+const HashFindNth = (linkedList, n) => {
+    let current = linkedList.head;
+
+    let hash = {};
+    let i = 0;
+
+    while(current){
+        hash[i] = current.value;
+        current = current.next;
+        i++;
+    }
+
+    //console.log(hash[n - 1]);
+    return hash[i - n];
 }
 
 let test1 = new linkedList();
@@ -36,6 +54,7 @@ test1.append(5);
 test1.append(6);
 
 
-console.log('Array: ', test1.toArray());
+//console.log('Array: ', test1.toArray());
+console.log('Brute Force: ', findNth(test1, 4));
 
-console.log('Test: ', findNth(test1, 4));
+console.log('HashTabkel:  ', HashFindNth(test1, 4));
