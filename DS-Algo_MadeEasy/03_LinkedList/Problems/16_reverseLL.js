304e9237e10021c9d1015d62cba7ac142d0c25b4
unknown <calwong42991@gmail.com>
2019-04-19 21:16:57 -0700

    reverse linked list complete

---
+++ DS-Algo_MadeEasy/03_LinkedList/Problems/16_reverseLL.js
@@ -0,0 +1,13 @@
+const reverseLL = (linkedList) => {
+    let head = linkedList.head;
+    let temp = null;
+    let nextNode = null;
+
+    while(head){
+        nextNode = head.next;
+        head.next = temp;
+        temp = head;
+        head = nextNode;
+    }
+    return temp;
+}
