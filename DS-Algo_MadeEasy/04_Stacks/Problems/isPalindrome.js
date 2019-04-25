2d8248830081802385d9624cc656996b0c73ee3a
unknown <calwong42991@gmail.com>
2019-04-24 22:40:24 -0700

    stack is palindrome complete

--- DS-Algo_MadeEasy/04_Stacks/Problems/isPalindrome.js
+++ DS-Algo_MadeEasy/04_Stacks/Problems/isPalindrome.js
@@ -1,3 +1,5 @@
+const Stack = require('../Stacks');
+
 const isPalindrome = (str) => {
     
     let i = 0;
@@ -15,4 +17,25 @@ const isPalindrome = (str) => {
         console.log('Palindrome');
         return 1;
     }
+}
+
+const isPalindrome1 = (str) => {
+    let i = 0;
+    let stack = new Stack();
+
+    while(str[i] !== 'X'){
+        stack.push(str[i]);
+        i++;
+    }
+
+    i++;
+    while(str[i]){
+        if(stack.isStackEmpty() || str[i] != stack.pop()){
+            console.log('Not a Palindrome');
+            return 0;
+        }
+        i++;
+    }
+
+    return stack.isStackEmpty();
 }
