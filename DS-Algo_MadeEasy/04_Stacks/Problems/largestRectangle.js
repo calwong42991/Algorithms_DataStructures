6cece03f19b4d04fa6c9ea974d03102afff1803c
unknown <calwong42991@gmail.com>
2019-04-26 19:13:50 -0700

    .

--- DS-Algo_MadeEasy/04_Stacks/Problems/largestRectangle.js
+++ DS-Algo_MadeEasy/04_Stacks/Problems/largestRectangle.js
@@ -1,11 +1,11 @@
 const largestRectangleAreaBruteForce = (histogram) => {
     let maxArea = 0;
 
-    for(let left = 0; left < histogram.length; left++){
-        for(let right = left; right < histogram.length; right++){
+    for (let left = 0; left < histogram.length; left++) {
+        for (let right = left; right < histogram.length; right++) {
             let min = Number.POSITIVE_INFINITY;
 
-            for(let i = left; i <= right; i++){
+            for (let i = left; i <= right; i++) {
                 min = Math.min(min, histogram[i]);
             }
 
@@ -37,6 +37,5 @@ const largestRectangleArea = (histogram) => {
     }
     return max;
 }
-
 
 console.log(largestRectangleAreaBruteForce([2, 1, 5, 6, 2, 3]));
