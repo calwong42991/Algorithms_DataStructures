33d843e7bf7ca9a92cb2748eba2b37de83b932ad
unknown <calwong42991@gmail.com>
2019-04-28 22:35:56 -0700

    .

--- DS-Algo_MadeEasy/04_Stacks/Problems/isPalindrome.js
+++ DS-Algo_MadeEasy/04_Stacks/Problems/isPalindrome.js
@@ -1,19 +1,19 @@
 const Stack = require('../Stacks');
 
 const isPalindrome = (str) => {
-    
+
     let i = 0;
     let j = str.length;
 
-    while( i < j && str[i] === str[j]){
+    while (i < j && str[i] === str[j]) {
         i++;
         j--;
     }
 
-    if(i < j){
+    if (i < j) {
         console.log('not palindrome');
         return 0;
-    } else{
+    } else {
         console.log('Palindrome');
         return 1;
     }
@@ -23,14 +23,14 @@ const isPalindrome1 = (str) => {
     let i = 0;
     let stack = new Stack();
 
-    while(str[i] !== 'X'){
+    while (str[i] !== 'X') {
         stack.push(str[i]);
         i++;
     }
 
     i++;
-    while(str[i]){
-        if(stack.isStackEmpty() || str[i] != stack.pop()){
+    while (str[i]) {
+        if (stack.isStackEmpty() || str[i] != stack.pop()) {
             console.log('Not a Palindrome');
             return 0;
         }
