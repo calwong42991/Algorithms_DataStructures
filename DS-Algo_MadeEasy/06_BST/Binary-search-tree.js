8e8843fd031060ee00664d846e0152de02d61aad
unknown <calwong42991@gmail.com>
2019-04-29 17:45:07 -0700

    pre,in,post order function complete

--- DS-Algo_MadeEasy/06_BST/Binary-search-tree.js
+++ DS-Algo_MadeEasy/06_BST/Binary-search-tree.js
@@ -1,11 +1,63 @@
 class Node {
-    constructor( value = null, left = null, right = null){
+    constructor(value = null, left = null, right = null) {
         this.value = value;
         this.left = left;
         this.right = right;
     }
-    
+
     toString() {
         return JSON.stringify(this);
     }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    printInOrder(process) {
+        let inOrder = (node) => {
+            if (node.left !== null) {
+                inOrder(node.left);
+            }
+
+            process.call(this, node);
+
+            if (node.right !== null) {
+                inOrder(node.right);
+            }
+        };
+        inOrder(this.root);
+    }
+
+    printPreOrder(process) {
+        let preOrder = (node) => {
+            process.call(this, node);
+            if (node.left !== null) {
+                preOrder(node.left);
+            }
+
+            if (node.right !== null) {
+                preOrder(node.right);
+            }
+        }
+        preOrder(this.root);
+    }
+
+    printPostOrder(process) {
+        let postOrder = (node) => {
+            if (node.left !== null) {
+                postOrder(node.left);
+            }
+
+            if (node.right !== null) {
+                postOrder(node.right);
+            }
+
+            process.call(this, node);
+        }
+
+        postOrder(this.root);
+    }
+
 }
