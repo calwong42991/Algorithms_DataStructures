a6b2bb88767a372b83bfa6336ca9a05394b58266
unknown <calwong42991@gmail.com>
2019-04-30 15:17:04 -0700

    hash map complete

---
+++ Algorithms/HashMap/HashMapBasic.js
@@ -0,0 +1,38 @@
+const hashStr = (str) => {
+    let finalHash = 0;
+
+    for(let i = 0; i < str.length; i++){
+        const charCode = str.charCodeAt(i);
+        finalHash += charCode;
+    }
+    return finalHash;
+}
+
+class HashMap {
+    constructor() {
+        this._storage = [];
+    }
+
+    set(key, val) {
+        let idx = this.hashStr(key);
+
+        if(!this._storage[idx]) {
+            this._storage[idx] = [];
+        }
+        this._storage[idx].push([key, val]);
+    }
+
+    get(key) {
+        let idx = this.hashStr(key);
+
+        if(!this._storage[idx]){
+            return undefined;
+        }
+
+        for(let keyVal of this._storage[idx]){
+            if(keyVal[0] === key){
+                return keyVal[1];
+            }
+        }
+    }
+}
