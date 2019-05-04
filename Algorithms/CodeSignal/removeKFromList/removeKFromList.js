5c38759e0f7e656a0976cb32fe23156930a31dbc
unknown <calwong42991@gmail.com>
2019-05-03 20:57:30 -0700

    remove k from list complete

---
+++ Algorithms/CodeSignal/removeKFromList/removeKFromList.js
@@ -0,0 +1,28 @@
+function removeKFromList(l, k) {
+    
+    let previous;
+    let current;
+    
+    
+    if(!l){
+        return l;
+    } else if(l.value === k){
+        l = l.next;
+    }
+    
+    current = l;
+    
+    while(current && current.next){
+        if(current.next.value === k){
+            current.next = current.next.next;
+        } else{
+            current = current.next;
+        }
+    }
+    
+    if(current !== null && current.value === k){
+        l = l.next;
+    }
+    
+    return l;
+}
