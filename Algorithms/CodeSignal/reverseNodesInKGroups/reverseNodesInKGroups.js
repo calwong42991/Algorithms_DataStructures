8f4450cbfbd14da6d61c95dd75f8505f8df63117
unknown <calwong42991@gmail.com>
2019-05-07 20:55:00 -0700

    .

--- Algorithms/CodeSignal/reverseNodesInKGroups/reverseNodesInKGroups.js
+++ Algorithms/CodeSignal/reverseNodesInKGroups/reverseNodesInKGroups.js
@@ -5,5 +5,41 @@
 // }
 //
 function reverseNodesInKGroups(l, k) {
-    
+    var list = null,
+        prevLast = null,
+        prev,
+        last,
+        tmp,
+        count = 0;
+
+    if(k === 1 ){
+        return l;
+    }
+
+    while(l){
+        prev = null;
+        last = l;
+
+        for(count; l && count < k; count++){
+            tmp = l.next;
+            l.next = prev;
+            prev = l;
+            l = tmp;
+        }
+
+        if(!list){
+            list = prev;
+        } else {
+            prevLast.next = prev;
+        }
+
+        if(i == k){
+            prevLast = last;
+            count = 0;
+        } else {
+            i = k - i;
+            l = prev;
+        }
+    }
+    return list;
 }
