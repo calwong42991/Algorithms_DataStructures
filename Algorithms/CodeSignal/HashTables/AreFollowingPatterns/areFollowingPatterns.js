11daa5298bca429411389d8604be6d95af320d83
unknown <calwong42991@gmail.com>
2019-05-11 12:53:15 -0700

    .

--- Algorithms/CodeSignal/HashTables/AreFollowingPatterns/areFollowingPatterns.js
+++ Algorithms/CodeSignal/HashTables/AreFollowingPatterns/areFollowingPatterns.js
@@ -1,19 +1,19 @@
 function areFollowingPatterns(strings, patterns) {
     let hash = {};
     let reverse = {};
-    
-    for(let i = 0; i < strings.length; i++){
+
+    for (let i = 0; i < strings.length; i++) {
         let p = patterns[i];
         let s = strings[i];
-        
-        if(!hash[p]){
+
+        if (!hash[p]) {
             hash[p] = s;
         }
-        
-        if(!reverse[s]) {
+
+        if (!reverse[s]) {
             reverse[s] = p;
-        } 
-        if(s !== hash[p] || reverse[s] !== p){
+        }
+        if (s !== hash[p] || reverse[s] !== p) {
             return false;
         }
     }
