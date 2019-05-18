a058e97be69875678badeab7a6a64daf44b26bf5
unknown <calwong42991@gmail.com>
2019-05-18 15:36:53 -0700

    .

--- DS-Algo_MadeEasy/02_Recursion_&_BackTracking/01_towerOfHanoi.js
+++ DS-Algo_MadeEasy/02_Recursion_&_BackTracking/01_towerOfHanoi.js
@@ -1,10 +1,10 @@
 const tower = (disk, source, destination, intermediate) => {
-    if(disk === 1){
-        console.log(`Move Disk 1 from ${source} to peg ${destination}`);
-        return;
-      } else {
-        tower(disk - 1, source, intermediate, destination);
-        console.log(`move disk ${disk} from ${source} to ${destination}`);
-        tower(disk - 1, intermediate, destination, source);
-      }
+  if (disk === 1) {
+    console.log(`Move Disk 1 from ${source} to peg ${destination}`);
+    return;
+  } else {
+    tower(disk - 1, source, intermediate, destination);
+    console.log(`move disk ${disk} from ${source} to ${destination}`);
+    tower(disk - 1, intermediate, destination, source);
+  }
 };
